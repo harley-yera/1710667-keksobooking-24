93b074de3128ee7a07f66e85f79c1b425068ac27
Yera <harley.yera@gmail.com>
2021-10-17 21:56:34 +0400

    Исправленные функции

--- js/main.js
+++ js/main.js
@@ -1,49 +1,25 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно.
-const randomInteger = (min, max) => {
-  if( min <= 0) {
-    return 'Неверный диапазон чисел';
-  }
-  const random = min + Math.random() * (max + 1 - min);
-  return Math.floor(random);
-};
-
-console.log(randomInteger(1, 5) );
-
-//Ошибка в const, правда? У него должно быть значение которое не будет меняться
-// C const вроде разобралась^^
-// Сейчас разбераюсь с if
-//Нужно ли писать else?
-
-// Вроде все норм сейчас :)
-
 const getRandomInt = (min, max) => {
-  if (min <= 0) {
+  if (min < 0 || min > max) {
     return 'Неверный диапазон чисел';
   }
+
   min = Math.ceil(min);
   max = Math.floor(max);
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+// eslint-disable-next-line no-console
 console.log(getRandomInt (1, 6));
 
 
-const getRandomNumber = (min, max) => {
-  if (min <= 0) {
-    return 'Неверный диапазон чисел';
-  }
-  return Math.random() * (max - min) + min;
-};
-
-console.log(getRandomNumber (1, 50));
-
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-
 const getRandomArbitrary = (min, max) => {
-  if (min < 0) {
+  if (min < 0 || min > max) {
     return 'Неверный диапазон чисел';
   }
+
   return (Math.random() * (max - min) + min).toFixed(1);
 };
 
+// eslint-disable-next-line no-console
 console.log(getRandomArbitrary (1, 50));
